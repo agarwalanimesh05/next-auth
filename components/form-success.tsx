367d86f16d5e3b6500cccc2cfb8ade476547595c
NAGARRO\animeshagarwal <animesh.agarwal@nagarro.com>
2024-08-10 11:34:29 -0700

    First commit

---
+++ components/form-success.tsx
@@ -0,0 +1,19 @@
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import React from "react";
+
+interface FormSuccessProps {
+  message?: string;
+}
+
+const FormSuccess = ({ message }: FormSuccessProps) => {
+  if (!message) return null;
+
+  return (
+    <div className="flex items-center gap-x-2 rounded-md bg-emerald-500/15 p-3 text-sm text-emerald-500">
+      <CheckCircledIcon className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default FormSuccess;
